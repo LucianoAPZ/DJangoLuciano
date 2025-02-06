5b51984976bf6e4a1efdee8a029f7aa78e1e922a
LucianoAPZ <lucianoanpoza@gmail.com>
2025-02-06 19:55:06 +0100

    Tabla Vote creada

--- polls/models.py
+++ polls/models.py
@@ -23,4 +23,11 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-    
+
+class Vote(models.Model):
+    id = models.AutoField(primary_key=True)
+    choice_id = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Vote {self.id} for choice {self.choice_id}
