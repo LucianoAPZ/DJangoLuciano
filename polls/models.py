89735c211766bdcf9f2dafdeadbaf228c4320940
LucianoAPZ <lucianoanpoza@gmail.com>
2025-02-06 19:55:53 +0100

    Tabla Vote creada2

--- polls/models.py
+++ polls/models.py
@@ -30,4 +30,4 @@ class Vote(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Vote {self.id} for choice {self.choice_id}
+        return f'Vote {self.id} for choice {self.choice_id}'
